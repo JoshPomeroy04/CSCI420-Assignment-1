7c61b460f25221e12a9976918339260c8eb5bd79
Josh Pomeroy <joshuaming2004@gmail.com>
2025-03-01 16:35:21 -0500

    Update 3 Sets

--- Split_Data.py
+++ Split_Data.py
@@ -11,17 +11,18 @@ with open(f"{FOLDER_PATH}/Cleaned_Corpus.txt", "r") as file:
         raw_corpus.append(line.strip())
 
 random.shuffle(raw_corpus)
+new_corpus = raw_corpus[:25000]
 
 with open(f"{FOLDER_PATH}/Training_Corpus.txt", "w") as file:
     for x in range(TRAINING_SIZE):
-        file.write(f"{raw_corpus.pop(0)}\n")
+        file.write(f"{new_corpus.pop(0)}\n")
 
 with open(f"{FOLDER_PATH}/Test_Set.txt", "w") as file:
     for x in range(TEST_SIZE):
-        file.write(f"{raw_corpus.pop(0)}\n")
+        file.write(f"{new_corpus.pop(0)}\n")
 
 with open(f"{FOLDER_PATH}/Eval_Set.txt", "w") as file:
     for x in range(EVAL_SIZE):
-        file.write(f"{raw_corpus.pop(0)}\n")
+        file.write(f"{new_corpus.pop(0)}\n")
 
 
