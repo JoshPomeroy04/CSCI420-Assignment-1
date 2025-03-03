cd8c799e4b29834625804185a5ba2d893fa662df
Josh Pomeroy <joshuaming2004@gmail.com>
2025-03-02 22:27:28 -0500

    Perplexity Calculation for all 3 models

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ lexer = JavaLexer()
 training_corpus = []
 eval_corpus = []
 tri_eval_set = []
-pent_eval_set = []
-non_eval_set = []
+penta_eval_set = []
+nona_eval_set = []
 tokenized_training_corpus = []
 words = []
 
@@ -34,8 +34,8 @@ for method in training_corpus:
 for method in eval_corpus:
     tokenized_method = [t[1].strip() for t in lexer.get_tokens(method) if t[1].strip() != '']
     tri_eval_set += ngrams(tokenized_method, 3)
-    pent_eval_set += ngrams(tokenized_method, 5)
-    non_eval_set += ngrams(tokenized_method, 9)
+    penta_eval_set += ngrams(tokenized_method, 5)
+    nona_eval_set += ngrams(tokenized_method, 9)
 
 def create_ngrams(tokenized_corpus, N):
     ngram = []
@@ -46,52 +46,38 @@ def create_ngrams(tokenized_corpus, N):
     return ngram_counts
 
 
-def calcTrigramPerp(bigram_Counts, trigram_Counts, tokens):   
-    bi_vocab_size = sum(bigram_Counts.values())
-    no_of_trigrams=0
+def calc_perp(n_minus_one_gram_Counts, ngram_Counts, N, eval_set):   
+    n_minus_one_vocab_size = sum(n_minus_one_gram_Counts.values())
+    no_of_ngrams=0
     prob_array = []
 
-    for trigram in tokens:
-        no_of_trigrams += 1
-        numerator = trigram_Counts[trigram] + 1
-        denominator = bigram_Counts[trigram[0:2]] + bi_vocab_size
+    for ngram in eval_set:
+        no_of_ngrams += 1
+        numerator = ngram_Counts[ngram] + 1
+        denominator = n_minus_one_gram_Counts[ngram[0:N-1]] + n_minus_one_vocab_size
         token_probability = log2(float(numerator) / float(denominator))
         prob_array.append(token_probability)
         
-    total_trigram_perplexity = np.exp(-np.mean(prob_array))
+    total_ngram_perplexity = np.exp(-np.mean(prob_array))
         
-    return total_trigram_perplexity
+    return total_ngram_perplexity
 
 bigrams = create_ngrams(tokenized_training_corpus, 2)
 trigrams = create_ngrams(tokenized_training_corpus, 3)
 
+quadgrams = create_ngrams(tokenized_training_corpus, 4)
 pentagrams = create_ngrams(tokenized_training_corpus, 5)
-nongrams = create_ngrams(tokenized_training_corpus, 9)
 
-trigram_perplexity = calcTrigramPerp(bigrams, trigrams, tri_eval_set)
+octagrams = create_ngrams(tokenized_training_corpus, 8)
+nonagrams = create_ngrams(tokenized_training_corpus, 9)
+
+trigram_perplexity = calc_perp(bigrams, trigrams, 3, tri_eval_set)
+pentagram_perplexity = calc_perp(quadgrams, pentagrams, 5, penta_eval_set)
+nonagram_perplexity = calc_perp(octagrams, nonagrams, 9, nona_eval_set)
 
 print(trigram_perplexity)
-
-"""
-print(trigram_model.vocab.lookup("public"))
-print(trigram_model.unmasked_score("public"))
-print(trigram_model.logscore("PagesByTestSystem"))
-print(trigram_model.logscore("partition"))
-print(trigram_model.logscore("("))
-print(trigram_model.logscore("Function"))
-print(trigram_model.logscore("<"))
-print(trigram_model.logscore("List"))
-print(trigram_model.logscore("WikiPage"))
-print(trigram_model.logscore("partition", ['public', 'PagesByTestSystem']))
-print(trigram_model.vocab.lookup(['>']))
-print(trigram_model.vocab.lookup(['public', 'PagesByTestSystem', 'partition', '(', 'Function', '<', 'List', '<', 'WikiPage']))
-"""
-# [self.logscore(ngram[-1], ngram[:-1]) for ngram in text_ngrams]
-
-#print(trigram_model.unmasked_score("public")+trigram_model.logscore("PagesByTestSystem")+trigram_model.logscore("partition")+trigram_model.logscore("(") + trigram_model.logscore("Function") + trigram_model.logscore("<") + trigram_model.logscore("List") + trigram_model.logscore("WikiPage") + trigram_model.logscore(">"))
-#print(pentgram_model.perplexity(test))
-
-#print(nongram_model.perplexity(test))
+print(pentagram_perplexity)
+print(nonagram_perplexity)
 
 
 
